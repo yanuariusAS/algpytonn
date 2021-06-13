bb645dba8e90fcb1213072ce941ec2d4c14477cd
yanuariusAS <85844901+yanuariusAS@users.noreply.github.com>
2021-06-14 02:08:26 +0700

    tugas algoritma dan pemograman

---
+++ cekKelulusan.py
@@ -0,0 +1,27 @@
+'''
+program cek kelulusan 
+NAMA:YANUARIUS ARDIAN SARDIN (20083000065)
+KELAS;2C
+'''
+import os
+clear = lambda : os.system('cls')
+jwb = "y"
+while jwb == "Y" or jwb == "y" :
+    clear()
+    print("PROGRAM CEK KELULUSAN")
+    print("---------------------")
+    print()
+    a = input("Masukkan Nilai = ")
+    n = int(a)
+    if n < 0 or n > 100 :
+        sts = "Inputan hanya boleh 0 sampai 100 saja"
+    elif n > 60 :
+        sts = "LULUS"
+    else :
+        sts = "TIDAK LULUS"
+    print()
+    print(sts)
+    print()
+    jwb = input("Cek lagi? (ya/tidak) : ")
+    if jwb == "t" or jwb == "T":
+        break
